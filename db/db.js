e7a77788fa9b608f00f7d1f8585ee74464258a8d
Harvel <harvelkay1@gmail.com>
2023-11-10 15:34:00 +0300

    Patched the connection string to the DB replaced it with an env variable

--- db/db.js
+++ db/db.js
@@ -6,15 +6,15 @@ const tracker = require("../log/logger");
 
 module.exports = async function () {
   try {
-    // const dev_email=config.get("dev_email")
-    // const dev_phone=config.get("dev_phone")
-    // const dev_pass=config.get("dev_pass")
+    const dev_email=config.get("dev_email")
+    const dev_phone=config.get("dev_phone")
+    const dev_pass=config.get("dev_pass")
     const dbUri = config.get("db_uri");
     mongoose.set("strictQuery", { tlsAllowInvalidCertificates:true });
-    mongoose.connect("mongodb://127.0.0.1/retail");
-    // await User.collection.insertOne({
-      // username:"harvel",email:dev_email,password:dev_pass,phone:dev_phone,isAdmin:true
-    // })
+    mongoose.connect(dbUri);
+    await User.collection.insertOne({
+      username:"harvel",email:dev_email,password:dev_pass,phone:dev_phone,isAdmin:true
+    })
     tracker.info(`Successfully Connected to DB....${dbUri}`);
   } catch (err) {
     tracker.error("Failed to connect to DB.....", err);

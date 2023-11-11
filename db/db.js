4696e8dad4f52d9df42cbf35ceab6f178394036f
Harvel <harvelkay1@gmail.com>
2023-11-11 11:09:29 +0300

    Thumbnail Patch
    Removed the public_url value from default.json and added it to the dev file

--- db/db.js
+++ db/db.js
@@ -1,18 +1,13 @@
 const { default: mongoose } = require("mongoose");
 const config = require("config");
 const tracker = require("../log/logger");
-const { User } = require("../models/user");
-
 
 module.exports = async function () {
   try {
 
     const dbUri = config.get("db_uri");
     mongoose.set("strictQuery", { tlsAllowInvalidCertificates:true });
-    mongoose.connect(dbUri);
-    await User.collection.insertOne({
-      username:"harvel",email:dev_email,password:dev_pass,phone:dev_phone,isAdmin:true
-    })
+    mongoose.connect(dbUri)
     tracker.info(`Successfully Connected to DB....${dbUri}`);
   } catch (err) {
     tracker.error("Failed to connect to DB.....", err);
